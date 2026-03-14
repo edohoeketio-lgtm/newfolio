fff361cdae6007d78715236da3d21c3b09757412
Maurice <edohoeketio@gmail.com>
2026-03-14 13:37:21 +0100

    fix: add article pages to vite rollup configuration to fix vercel 404

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,9 @@ export default defineConfig({
         main: resolve(__dirname, 'index.html'),
         clips: resolve(__dirname, 'pages/clips/index.html'),
         ghost: resolve(__dirname, 'pages/ghost/index.html'),
+        articles_index: resolve(__dirname, 'pages/articles/index.html'),
+        articles_ai_workflow: resolve(__dirname, 'pages/articles/ai-orchestration-workflow.html'),
+        articles_streamfm: resolve(__dirname, 'pages/articles/streamfm.html')
       }
     }
   }
